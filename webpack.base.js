ff2a2bad1f83d7d410dd903737a34952e897673d
Denllay <denllaay@gmail.com>
2021-10-10 16:43:55 +0300

    feat: change tsconfig, add eslint

--- webpack.base.js
+++ webpack.base.js
@@ -2,6 +2,8 @@ const path = require('path');
 const ESLintPlugin = require('eslint-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
+const { isJSDocFunctionType } = require('typescript');
+const { join } = require('path');
 
 module.exports = {
   module: {
@@ -42,11 +44,17 @@ module.exports = {
   resolve: {
     extensions: ['.tsx', '.ts', '.js'],
     alias: {
-      '@': path.resolve(__dirname, 'src'),
+      '@app': path.resolve(__dirname, 'src/app/'),
+      '@pages': path.resolve(__dirname, 'src/pages/'),
+      '@widgets': path.resolve(__dirname, 'src/widgets/'),
+      '@features': path.resolve(__dirname, 'src/features/'),
+      '@entities': path.resolve(__dirname, 'src/entities/'),
+      '@shared': path.resolve(__dirname, 'src/shared/'),
     },
   },
+
   plugins: [
-    // ? Очистка говна
+    // ? Очистка
     new CleanWebpackPlugin(),
     // ? Eslint
     new ESLintPlugin({
